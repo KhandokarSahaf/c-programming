ccbbf2c90dac70ec26094232d23bc6b781033a9e
KhandokarSahaf <khandokarsahaf3@gmail.com>
2025-08-12 15:54:08 +0600

    first commit

---
+++ 1st.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+int sahaf_id;
+char sahaf_addr[] = "Jatrabari";
+sahaf_id = 25205038;
+
+printf ("This is a uniqe id for Sahaf: %d\n", sahaf_id);
+printf ("This is my location: %s\n", sahaf_addr);
+
+return 0;
+}
---
+++ progamming.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (){
+int defined_var;
+printf ("\tDefined_var: %d\n\t", defined_var);
+// assinged a value
+defined_var = 12;
+int int_number = 25205038;
+printf ("Value of defined var after assingment: %d\n\t", defined_var);
+printf("value of int number is : %d\t\n", int_number);
+return 0;
+}
