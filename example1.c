2e17a01bf1c794d32e42bb6b19fafd7eb34328da
KhandokarSahaf <khandokarsahaf3@gmail.com>
2025-08-16 19:43:21 +0600

    example c

---
+++ example1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+char name[] = "sahaf";
+char addr[] = "Jatrabari";
+int salary =  0;
+
+int x (){
+    printf("%s\n", name);
+    printf("%s\n", addr);
+    printf("%d", salary);
+
+}
+
+int main () {
+    x ();
+    return 0;
+}
