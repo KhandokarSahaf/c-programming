d8895f160cf3ec545b616e1e65e146137b0c3cac
KhandokarSahaf <khandokarsahaf3@gmail.com>
2025-08-16 18:41:20 +0600

    c

---
+++ 2nd.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int defined_var = 25205038;
+int a = 20;
+int sah ();
+
+int main (){
+sah();
+return 0;
+}
+
+int sah (){
+    printf("%d\n", defined_var);
+    printf("%d\n", a);
+    return 0;
+}
